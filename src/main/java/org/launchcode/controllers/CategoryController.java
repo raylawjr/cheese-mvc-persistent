f21562e3b35d3d256516a0f8a21557fed265810f
Dan Ray <raylawjr@gmail.com>
2017-04-11 01:18:31 -0500

    finished part 1 of Cheese-MVC-Persistent

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,27 +1,53 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by melissa on 4/10/17.
  */
 
 @Controller
-@RequestMapping("controller")
+@RequestMapping("category")
 public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
 
-    @RequestMapping("")
+    @RequestMapping(value="")
     public String index(Model model) {
 
         model.addAttribute("categories", categoryDao.findAll());
         model.addAttribute("title", "Categories");
+
         return "category/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddCategoryForm(Model model) {
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+        return "category/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String processAddCategoryForm(@ModelAttribute @Valid Category newCategory, Errors errors, Model model) {
+
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+        }
+
+        categoryDao.save(newCategory);
+        return "redirect:";
     }
 }
